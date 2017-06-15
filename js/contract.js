25f3cbdf4475b214ddf129cc1eaf2bc10f832cde
Daniel Mermelstein <danmermel@gmail.com>
2017-06-15 08:42:41 +0100

    added single solidity file.. modified pages to take that

--- js/contract.js
+++ js/contract.js
@@ -12,58 +12,6 @@ function hextoascii(str1) {
   return str;  
 };
 
-var abi = [{
-    constant: true,
-    inputs: [],
-    name: "name",
-    outputs: [{
-        name: "",
-        type: "bytes32"
-    }],
-    payable: false,
-    type: "function"
-}, {
-    constant: true,
-    inputs: [],
-    name: "hash",
-    outputs: [{
-        name: "",
-        type: "bytes32"
-    }],
-    payable: false,
-    type: "function"
-}, {
-    constant: true,
-    inputs: [],
-    name: "owner",
-    outputs: [{
-        name: "",
-        type: "address"
-    }],
-    payable: false,
-    type: "function"
-}, {
-    constant: true,
-    inputs: [],
-    name: "timestamp",
-    outputs: [{
-        name: "",
-        type: "uint256"
-    }],
-    payable: false,
-    type: "function"
-}, {
-    inputs: [{
-        name: "_hash",
-        type: "bytes32"
-    }, {
-        name: "_name",
-        type: "bytes32"
-    }],
-    payable: true,
-    type: "constructor"
-}];
-
 var app = new Vue({
   el: '#app',
   data: {
